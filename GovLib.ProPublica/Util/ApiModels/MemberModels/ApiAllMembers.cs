b9cb000fb5095c2e83326f430a9cfaa46769f453
Phil H <ph-hawkins@outlook.com>
2018-01-06 16:17:01 -0500

    Doug Jones broke everything

--- GovLib.ProPublica/Util/ApiModels/MemberModels/ApiAllMembers.cs
+++ GovLib.ProPublica/Util/ApiModels/MemberModels/ApiAllMembers.cs
@@ -38,39 +38,45 @@ namespace GovLib.ProPublica.Util.MemberModels
         {
             Politician pol;
 
-            if (entity == null)
-                return null;
+            if (entity == null) return null;
             
-            if (chamber == Chamber.Senate)
-                pol = new Senator();
-            else
-                pol = new Representative();
+            if (chamber == Chamber.Senate) pol = new Senator();
+            else pol = new Representative();
 
             pol.CongressID = entity.id;
             pol.FirstName = entity.first_name;
             pol.LastName = entity.last_name;
             pol.Party = entity.party;
             pol.State = (State) EnumConvert.StateCodeToEnum(entity.state);
-            pol.Seniority = (int) entity.seniority;
+
             pol.OcdID = entity.ocd_id;
             pol.BirthDate = DateTime.ParseExact(entity.date_of_birth, "yyyy-MM-dd", CultureInfo.InvariantCulture);
             pol.InOffice = (bool) entity.in_office;
-            pol.MissedVotesRatio = (double) entity.missed_votes_pct / 100;
-            pol.PartyLoyaltyRatio = (double) entity.votes_with_party_pct / 100;
-            pol.VotesCast = (int) entity.total_votes;
-            pol.VotesMissed = (int) entity.missed_votes;
-            pol.VotesPresent = (int) entity.total_present;
+
+            if (entity.seniority == null) pol.Seniority = 0;
+            else pol.Seniority = (int) entity.seniority;
+
+            if (entity.missed_votes_pct == null) pol.MissedVotesRatio = 0;
+            else pol.MissedVotesRatio = (double) entity.missed_votes_pct / 100;
+
+            if (entity.votes_with_party_pct == null) pol.PartyLoyaltyRatio = 0;
+            else pol.PartyLoyaltyRatio = (double) entity.votes_with_party_pct / 100;
+
+            if (entity.total_votes == null) pol.VotesCast = 0;
+            else pol.VotesCast = (int) entity.total_votes;
+
+            if (entity.missed_votes == null) pol.VotesMissed = 0;
+            else pol.VotesMissed = (int) entity.missed_votes;
             
-            if (entity.gender == "M")
-                pol.Gender = Gender.Male;
-            else
-                pol.Gender = Gender.Female;
+            if (entity.total_present == null) pol.VotesPresent = 0;
+            else pol.VotesPresent = (int) entity.total_present;
+            
+            if (entity.gender == "M") pol.Gender = Gender.Male;
+            else pol.Gender = Gender.Female;
 
-            if (entity.next_election != null)
-                pol.NextElection = Int32.Parse(entity.next_election);
+            if (entity.next_election != null) pol.NextElection = Int32.Parse(entity.next_election);
 
-            if (!string.IsNullOrEmpty(entity.middle_name))
-                pol.MiddleName = entity.middle_name;
+            if (!string.IsNullOrEmpty(entity.middle_name)) pol.MiddleName = entity.middle_name;
 
             return pol;
         }
