809fa1f0819b34e646ea5380285b4738a01628df
Phil H <ph-hawkins@outlook.com>
2018-04-17 02:25:17 -0400

    Ignore "No such file or directory" when removing old test data

--- clone-test-data.sh
+++ clone-test-data.sh
@@ -5,6 +5,6 @@ set -e
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 TEST_DATA_PATH="$SCRIPT_PATH/GovLib.TestData"
 
-rm -r "$TEST_DATA_PATH"
+rm -rf "$TEST_DATA_PATH"
 
 git clone https://github.com/GovLib/GovLib.TestData "$TEST_DATA_PATH"
